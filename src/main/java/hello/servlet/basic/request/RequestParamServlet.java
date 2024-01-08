7ab54640a49032795fe9d2cd03ee9f31461721e4
nickhealthy <alskadmlcraz1@gmail.com>
2024-01-09 00:46:48 +0900

    서블릿 - HttpServletRequest 객체 사용 HTTP 요청 데이터 - POST HTML Form

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -22,6 +22,14 @@ import java.util.Map.Entry;
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
+    /*
+        request.getParameter()는 GET URL 쿼리 파라미터 형식도 지원하고,
+        POST HTML Form 형식도 지원한다.
+
+        GET URL 쿼리 파라미터 형식으로 클라이언트에서 서버로 데이터를 전달할 땐 HTTP 메시지 바디를 사용하지 않기 때문에 content-type이 없다.(null)
+        POST HTML Form 형식으로 데이터를 전달함녀 HTTP 메시지 바디에 해당 데이터를 포함해서 보내기 때문에 바디에 포함된 데이터가 어떤 형식인지 content-type을 꼭 지정해야 한다.
+        이렇게 폼으로 데이터를 전송하는 형식을 'application/x-www-form-urlencoded' 라고 한다.
+     */
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
