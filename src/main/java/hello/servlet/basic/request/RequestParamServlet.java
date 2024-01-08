2c89af64c5e6eeecea2a215ee478295b4da540ff
nickhealthy <alskadmlcraz1@gmail.com>
2024-01-09 00:26:01 +0900

    서블릿 - HttpServletRequest 객체 사용 HTTP 요청 데이터 - GET 쿼리 파라미터

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,70 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ *
+ * 2. 동일한 파라미터 전송 가능
+ * http://localhost:8080/request-param?username=hello&username=kim&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+
+        // 옛날 방식
+//        Enumeration<String> parameterNames = request.getParameterNames();
+//        while (parameterNames.hasMoreElements()) {
+//            String paramName = parameterNames.nextElement();
+//            System.out.println("paramName = " + paramName);
+//        }
+
+        request.getParameterNames().asIterator()
+                        .forEachRemaining(paramName -> System.out.println(
+                                "paramName = " + paramName));
+
+        // 파라미터를 Map으로 조회
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Entry<String, String[]> stringEntry : parameterMap.entrySet()) {
+            System.out.println("stringEntry.getKey() = " + stringEntry.getKey());
+            System.out.println("stringEntry.getValue() = " + Arrays.toString(stringEntry.getValue()));
+
+        }
+
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        System.out.println("request.getParameter(username) = " + username);
+
+        String age = request.getParameter("age");
+        System.out.println("request.getParameter(age) = " + age);
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username = " + name);
+        }
+
+
+        response.getWriter().write("OK");
+
+    }
+}
