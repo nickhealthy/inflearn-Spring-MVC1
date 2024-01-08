a2ee6a14362d87a2aa260a6e959a0709302a21e8
nickhealthy <alskadmlcraz1@gmail.com>
2024-01-08 21:08:30 +0900

    서블릿 - HttpServletRequest 객체 사용 - 헤더 정보

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -15,6 +15,8 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
+    // 서블릿은 개발자가 HTTP 요청 메시지를 편리하게 사용할 수있도록 개발자 대신 HTTP 요청 메시지를 파싱한다.
+    // 그리고 그 결과를 HTTPServletRequest 객체에 담아 제공한다.
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
