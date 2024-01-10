2af7d09f05395d880adc231c52f0883b29fa5795
nickhealthy <alskadmlcraz1@gmail.com>
2024-01-10 17:14:21 +0900

    MVC 프레임워크 만들기 - 프론트 컨트롤러 도입 - v1

---
+++ src/main/java/hello/servlet/web/frontcontroller/controller/MemberSaveControllerV1.java
@@ -0,0 +1,42 @@
+package hello.servlet.web.frontcontroller.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 회원 저장 컨트롤러
+ */
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+  private MemberRepository memberRepository = MemberRepository.getInstance();
+
+  @Override
+  public void process(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    String username = request.getParameter("username");
+    int age = Integer.parseInt(request.getParameter("age"));
+
+    Member member = new Member(username, age);
+    System.out.println("member = " + member);
+    memberRepository.save(member);
+
+     /*
+       Model에 데이터를 보관한다.
+       request는 내부에 데이터 저장소를 가지고 있다.(MAP 구조)
+       request.setAttribute(), request.getAttribute()를 사용하면 데이터를 보관하고, 조회할 수 있다.
+     */
+    request.setAttribute("member", member);
+
+    String viewPath = "/WEB-INF/views/save-result.jsp";
+    RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+    dispatcher.forward(request, response);
+
+  }
+}
